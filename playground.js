03694765fdaf8a04e09a679201b08252135dc7da
zoe :] <zoe.lx7@gmail.com>
2022-12-08 01:54:17 -0800

    save & load buttons

--- playground.js
+++ playground.js
@@ -1,8 +1,21 @@
 let counter = document.getElementById("counter");
 let result = document.getElementById("result");
+let save = document.getElementById("save");
+let load = document.getElementById("load");
 let count = 0;
 
 counter.addEventListener("click", () => {
     count += 1;
     result.innerHTML = count
 })
+
+save.addEventListener("click", () => {
+    localStorage.setItem("count", count)
+})
+
+load.addEventListener("click", () => {
+    count = localStorage.getItem("count");
+    count = parseInt(count);
+    document.getElementById("text").value = count;
+}
+)
